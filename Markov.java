ec876f77ada19663b328eae3db747a667cf52222
Charles Stremel <cstremel@csumb.edu>
2025-03-12 15:28:40 -0700

    All methods done. String out of range error.

--- Markov.java
+++ Markov.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Collection;
+import java.util.Random;
 
 public class Markov {
 	private static String BEGINS_SENTENCE;	
@@ -19,20 +21,47 @@ public class Markov {
 		words = new HashMap<>();
 	}
 
+	public HashMap <String, ArrayList <String>> getWords() {
+		return words;
+	}
+
+	public String toString() {
+		return words.toString();
+	}
+
 	String randomWord(String currentHash) {
-		System.out.println("randomWord");
-		System.out.println(words.get(currentHash));
+		int size = words.get(currentHash).size();
+		Random r = new Random();
+		int ran = r.nextInt(size);
 		
-		//String currentWord = "";
-		//String [] tmp = words.size(
-		//return currentWord;
-		
-		return "";	
+		Collection <String> hashWords = words.get(currentHash);
+	
+		String currentWord = hashWords.toArray()[ran].toString();
+
+		return currentWord;	
 	}
 
 	public String getSentence() {
-		this.randomWord(BEGINS_SENTENCE);	
-		return "";
+		String currentWord  = randomWord(BEGINS_SENTENCE);	
+		StringBuilder sb = new StringBuilder();
+
+		Random r = new Random();
+
+		while(!endsWithPunctuation(currentWord)) {
+			sb.append(currentWord).append(" ");
+			
+			Collection <String> keyWords = words.keySet();
+			int size = keyWords.size();
+			int ran = r.nextInt(size);
+			
+			String newWord = keyWords.toArray()[ran].toString();
+			//System.out.println(newWord);
+			currentWord = randomWord(newWord);
+		}
+		if(endsWithPunctuation(currentWord)) {
+			sb.append(currentWord);
+		}
+		return sb.toString();
 	}
 
 	public static boolean endsWithPunctuation(String currentLine) {
@@ -64,23 +93,24 @@ public class Markov {
 
 	void addLine(String currentLine) {
 		if(currentLine.length() == 0) {
-			System.out.println("Zero Line");
+			System.out.println("Empty Line");
 		} else {
 			
-			System.out.print(currentLine + " ");
-			System.out.println(currentLine.length());
+			//System.out.print(currentLine + " ");
+			//System.out.println(currentLine.length());
 			
 			String [] words = currentLine.split(" ");
-			System.out.println(words.length);
+			//System.out.println(words.length);
 			prevWord = "X";
 
 			for(int i=0; i<words.length; i++) {
-				System.out.println("Adding " + words[i]);
+				//System.out.println("Adding " + words[i]);
 				this.addWord(words[i]);
 			}
 		}
-		System.out.println(words);
-		System.out.println(endsWithPunctuation(currentLine));
+		//System.out.println(words);
+		//System.out.println(endsWithPunctuation(currentLine));
+		//System.out.println(getSentence());
 	}
 
 	public void addFromFile(String filename) {
@@ -96,14 +126,23 @@ public class Markov {
 			System.out.printf("Could not read %s $n", filename);
 			return;
 		} finally {
-			System.out.println("Successfuly captured words.");
+			System.out.println("Done.");
 			return;
 		}
 	}
 
 	public static void main(String [] args) {
 		Markov markov = new Markov();
-		markov.addFromFile("blan.txt");
+		
+		Scanner sIn = new Scanner(System.in);
+		System.out.print("Enter filename: ");
+		String filename = sIn.next();
+		
+		markov.addFromFile(filename);
+		
+		System.out.println(markov);
+		System.out.println(markov.getWords());
+		System.out.println(markov.getSentence());
 	}
 }
 
